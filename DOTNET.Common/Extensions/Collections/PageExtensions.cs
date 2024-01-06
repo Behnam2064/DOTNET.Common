e4a5e907049f1ca0c2c8af7cdbf02bbaa0522632
administrator <172.19.0.5\Doctors$>
2024-01-06 13:05:58 +0330

    IQueryable Page Extensions added

---
+++ DOTNET.Common/Extensions/Collections/PageExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET.Common.Extensions.Collections
+{
+    public static class PageExtensions
+    {
+        public static IQueryable<T> Page<T>(this IQueryable<T> Query, int Page, int PageSize)
+        {
+            int skip = Math.Max(PageSize * (Page - 1), 0);
+            return Query.Skip(skip).Take(PageSize);
+        }
+    }
+}
