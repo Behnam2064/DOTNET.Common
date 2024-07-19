6fd79636328a258882c6e33e6c6be6e0eddbc324
Behnam2064 <behnamgraphic3@gmail.com>
2024-07-19 00:17:59 -0700

    feat: The DateTime extensions has been added

---
+++ DOTNET.Common/Extensions/DateTimes/DateTimeExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET.Common.Extensions.DateTimes
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime TrimMilliseconds(this DateTime dateTime)
+        {
+            return dateTime.AddMicroseconds(-dateTime.Microsecond);
+        }
+
+        public static DateTime TrimSeconds(this DateTime dateTime)
+        {
+            return dateTime.AddSeconds(-dateTime.Second);
+        }
+
+        public static DateTime TrimMinutes(this DateTime dateTime)
+        {
+            return dateTime.AddMinutes(-dateTime.Minute);
+        }
+
+        public static DateTime TrimToMinutes(this DateTime dateTime)
+        {
+            return dateTime.TrimMilliseconds().TrimMinutes();
+        }
+
+        public static bool IsToday(this DateTime dateTime)
+        {
+            return dateTime.Year == DateTime.Now.Year && dateTime.Month == DateTime.Now.Month && dateTime.Day == DateTime.Now.Day;
+        }
+
+        public static double GetMilliseconds(this DateTime start, DateTime end) => end.Subtract(start).TotalMilliseconds;
+
+        public static double GetSeconds(this DateTime start, DateTime end) => end.Subtract(start).TotalSeconds;
+
+        public static double GetMinutes(this DateTime start, DateTime end) => end.Subtract(start).TotalMinutes;
+
+        public static double GetHours(this DateTime start, DateTime end) => end.Subtract(start).TotalHours;
+
+        public static double GetDays(this DateTime start, DateTime end) => end.Subtract(start).TotalDays;
+
+    }
+}
