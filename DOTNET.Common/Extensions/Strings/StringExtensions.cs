3105eb0b59aa7bd12ca031b243228e35d613c7fe
administrator <172.19.0.5\Doctors$>
2024-08-12 11:20:37 +0330

    feat:The extensions to remove illegal characters for a file name has been created

---
+++ DOTNET.Common/Extensions/Strings/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DOTNET.Common.Extensions.Strings
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Remove illegal characters for a file name
+        /// </summary>
+        /// <param name="FileName">Your file name</param>
+        /// <param name="Replace">Alternate character</param>
+        /// <returns>String without illegal characters</returns>
+        public static string RemoveIllegalCharacters(this string FileName, char Replace)
+        {
+
+            if (string.IsNullOrEmpty(FileName) || string.IsNullOrWhiteSpace(FileName))
+                return FileName;
+
+            // length file name exp
+
+            char[] Illegal = { '\\', '/', ':', '*', '?', '"', '<', '>', '|' };
+
+            if (Illegal.Any(x => x == Replace))
+                throw new System.IO.IOException("Illegal file name");
+
+            if (!Illegal.Any(x => FileName.Contains(x)))
+                return FileName;
+
+            foreach (char item in Illegal)
+            {
+                FileName = FileName.Replace(item, Replace);
+            }
+
+            return FileName;
+
+        }
+    }
+}
