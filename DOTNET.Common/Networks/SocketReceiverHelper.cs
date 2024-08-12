4bbf9ee4cb622653cb107932879213ecb7063a70
administrator <172.19.0.5\Doctors$>
2024-08-12 11:19:13 +0330

    feat:The SocketReceiverHelper class was optimized to receive files

--- DOTNET.Common/Networks/SocketReceiverHelper.cs
+++ DOTNET.Common/Networks/SocketReceiverHelper.cs
@@ -1,6 +1,7 @@
 ﻿using DOTNET.Common.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
@@ -24,22 +25,26 @@ namespace DOTNET.Common.Networks
         /// <param name="ws"></param>
         /// <param name="arguments"></param>
         /// <returns></returns>
-        public async Task<byte[]> GetAsBytesAsync(SocketReceiverArguments arguments)
+        public async Task<SocketReceivedMessageResult> GetAsBytesAsync(SocketReceiverArguments arguments)
         {
             var compoundBuffer = new List<byte>();
             var messageReceiveResult = new WebSocketReceiveResult(0, WebSocketMessageType.Close, false);
-            byte[] buffer = new byte[arguments.BufferSize];
-
+            //byte[] buffer = new byte[arguments.BufferSize];
+            SocketReceivedMessageResult result = new SocketReceivedMessageResult() 
+            {
+                Result = new byte[arguments.BufferSize],
+            };
             while (!messageReceiveResult.EndOfMessage)
             {
                 // read one message until the end
-                messageReceiveResult = await arguments.WebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), arguments.CancellationToken);
-                if (messageReceiveResult.MessageType == arguments.WebSocketMessageType)
-                {
-                    byte[] readBytes = new byte[messageReceiveResult.Count];
-                    Array.Copy(buffer, readBytes, messageReceiveResult.Count);
-                    compoundBuffer.AddRange(readBytes);
-                }
+                messageReceiveResult = await arguments.WebSocket.ReceiveAsync(new ArraySegment<byte>(result.Result), arguments.CancellationToken);
+
+                result.WebSocketMessageType = messageReceiveResult.MessageType;
+
+                byte[] readBytes = new byte[messageReceiveResult.Count];
+                Array.Copy(result.Result, readBytes, messageReceiveResult.Count);
+                compoundBuffer.AddRange(readBytes);
+
 
                 /*if (messageReceiveResult.MessageType == WebSocketMessageType.Text)
                 {
@@ -49,8 +54,9 @@ namespace DOTNET.Common.Networks
                 }*/
             }
             // get string message
-
-            return compoundBuffer.ToArray();
+            //result.Result = buffer;
+            result.Result = compoundBuffer.ToArray();
+            return result; //compoundBuffer.ToArray();
         }
 
         /// <summary>
@@ -59,7 +65,19 @@ namespace DOTNET.Common.Networks
         /// <param name="arguments"></param>
         /// <returns></returns>
         public async Task<string> GetAsStringAsync(SocketReceiverArguments arguments)
-            => await Task.Run(async () => arguments.Encoding.GetString(await GetAsBytesAsync(arguments)));
+            => await Task.Run(async () =>
+            {
+                
+                var result = await GetAsBytesAsync(arguments);
+/*
+                if(result.WebSocketMessageType != WebSocketMessageType.Text)
+                {
+                    throw new InvalidOperationException($"Cannot convert a binary file to text.(Received WebSocketMessageType:{result.WebSocketMessageType})");
+                }*/
+
+
+                return arguments.Encoding.GetString(result.Result);
+            });
 
         /// <summary>
         /// An encrypted string is sent to this method
@@ -67,16 +85,29 @@ namespace DOTNET.Common.Networks
         /// <param name="arguments"></param>
         /// <returns></returns>
         public async Task<string> GetISecurityTextAsStringAsync(SocketReceiverArguments arguments)
-            => await Task.Run(async () => arguments.ISecurityText.DecryptText(arguments.Encoding.GetString(await GetAsBytesAsync(arguments))));
+            => await Task.Run(async () => 
+            {
+                var result = await GetAsBytesAsync(arguments);
+
+                /*
+                if (result.WebSocketMessageType != WebSocketMessageType.Text)
+                {
+                    throw new InvalidOperationException($"Cannot convert a binary file to text.(Received WebSocketMessageType:{result.WebSocketMessageType})");
+                }
+                */
+
+                return arguments.ISecurityText.DecryptText(arguments.Encoding.GetString(result.Result));
+
+            });
 
         public async Task<TEntity?> GetEntityAsync<TEntity>(SocketReceiverArguments arguments) where TEntity : class
-        => await Task.Run(async () =>JsonSerializer.Deserialize<TEntity>(await GetAsStringAsync(arguments)));
+        => await Task.Run(async () => JsonSerializer.Deserialize<TEntity>(await GetAsStringAsync(arguments)));
 
         public async Task<TEntity?> GetISecurityTextAsEntityAsync<TEntity>(SocketReceiverArguments arguments) where TEntity : class
         => await Task.Run(async () =>
         {
             string txt = await GetAsStringAsync(arguments);
-            if(string.IsNullOrEmpty(txt) || string.IsNullOrWhiteSpace(txt)) return null;    
+            if (string.IsNullOrEmpty(txt) || string.IsNullOrWhiteSpace(txt)) return null;
             txt = arguments.ISecurityText.DecryptText(txt);
             return JsonSerializer.Deserialize<TEntity>(txt);
 
@@ -84,6 +115,11 @@ namespace DOTNET.Common.Networks
         });
     }
 
+    public class SocketReceivedMessageResult
+    {
+        public WebSocketMessageType WebSocketMessageType { get; set; }
+        public byte[]? Result { get; set; }
+    }
 
     public class SocketReceiverArguments
     {
@@ -91,7 +127,7 @@ namespace DOTNET.Common.Networks
 
         public required CancellationToken CancellationToken;
         public required WebSocket WebSocket { get; set; }
-        public required WebSocketMessageType WebSocketMessageType { get; set; }
+        //public required WebSocketMessageType WebSocketMessageType { get; set; }
         public ISecurityText? ISecurityText { get; set; }
         public Encoding Encoding { get; set; }
 
