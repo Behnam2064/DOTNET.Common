5be1dc44c43832f6abf27c750fa02aac1504a952
Behnam2064 <behnamgraphic3@gmail.com>
2024-08-18 10:37:33 +0330

    feat:Stream extensions (Save as file)

---
+++ DOTNET.Common/Extensions/Streams/StreamExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET.Common.Extensions.Streams
+{
+    public static class StreamExtensions
+    {
+        public static void Save(this Stream stream, string file)
+        {
+            using (var fileStream = File.Create(file))
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.CopyTo(fileStream);
+            }
+        }
+    }
+}
