2966d0a806358ac7967748ca66f5eeea7b06f169
Behnam2064 <Behnam@DESKTOP-0PJLIOQ>
2025-07-02 12:10:07 +0330

    feat:Easy search and filtering in Actions

---
+++ DOTNET.Common/Extensions/IQueryables/IQueryableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET.Common.Extensions.IQueryables
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> ApplySorting<T>(this IQueryable<T> source, string orderBy, string sortDirection)
+        {
+            if (string.IsNullOrWhiteSpace(orderBy))
+                return source;
+
+            var type = typeof(T);
+            var property = type.GetProperty(orderBy);
+            if (property == null)
+                return source;
+
+            var parameter = Expression.Parameter(type, "x");
+            var selector = Expression.Property(parameter, property);
+            var lambda = Expression.Lambda(selector, parameter);
+
+            string method = sortDirection.ToLower() == "ascending" ? "OrderBy" : "OrderByDescending";
+
+            var result = typeof(Queryable).GetMethods()
+                .Where(m => m.Name == method && m.GetParameters().Length == 2)
+                .Single()
+                .MakeGenericMethod(type, property.PropertyType)
+                .Invoke(null, new object[] { source, lambda });
+
+            return (IQueryable<T>)result!;
+        }
+
+        public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            return query.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+        }
+    }
+
+}
